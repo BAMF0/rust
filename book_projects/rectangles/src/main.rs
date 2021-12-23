74e95d4443e829c84607f0084f57e18d7cb22690
Simon Johnsson <simjohn@student.chalmers.se>
2021-12-23 17:17:42 +0100

    Finishes the struct chapter

---
+++ book_projects/rectangles/src/main.rs
@@ -0,0 +1,105 @@
+fn main() {
+    let width1 = 30; 
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels. 
+        (Using two variables)",
+        area(width1, height1)
+    );
+
+    let rect1 = (30,50);
+
+
+    println!(
+        "The area of the rectangle is {} square pixels.
+        (Using tuples)",
+        area_t(rect1)
+    );
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.
+        (Using structs)",
+        area_s(&rect1)
+    );
+
+    println!("rect1 is {:?}",rect1);
+
+    println!("rect1 is {:#?}",rect1);
+
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+
+    println!(
+        "The are of the rectangle {} square pixels.
+        (Using methods)",
+        rect1.area()
+    );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width;  it is {}", rect1.width);
+    }
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);    
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle { // Associated functions to the rectangle type
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle { // Like static method in java
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_t(dimensions: (u32,u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_s(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
