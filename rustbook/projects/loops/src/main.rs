a3b3aa0672b5a45df810086f80eec72cbadaf723
Simon Johnsson <simzter@outlook.com>
2022-10-11 17:23:13 +0200

    Add loops

---
+++ rustbook/projects/loops/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+    
+    for number in 1..21 {
+        println!("Fibonacci of {} = {}", number, fib(number));
+    }
+}
+
+// Return the n:th number in the fibonnaci sequence
+fn fib(n: i32) -> i32 {
+    match n {
+        1 => 1,
+        2 => 1,
+        _ => fib(n-1) + fib(n-2)
+    }
+}
